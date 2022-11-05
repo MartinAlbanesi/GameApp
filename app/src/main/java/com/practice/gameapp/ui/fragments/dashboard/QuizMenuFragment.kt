1197ed784ce71a4ffae1aafc75c6d31dc9a939a5
Martín Albanesi <martinalbanesi89@gmail.com>
2022-11-05 16:03:01 -0300

    renamed fragment and viewmodel

--- app/src/main/java/com/practice/gameapp/ui/fragments/dashboard/QuizMenuFragment.kt
+++ app/src/main/java/com/practice/gameapp/ui/fragments/dashboard/QuizMenuFragment.kt
@@ -5,14 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.practice.gameapp.databinding.FragmentDashboardBinding
-import com.practice.gameapp.ui.viewmodels.DashboardViewModel
+import com.practice.gameapp.ui.viewmodels.HomeViewModel
+import com.practice.gameapp.ui.viewmodels.QuizViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DashboardFragment : Fragment() {
+class QuizMenuFragment : Fragment() {
 
+    //ViewModel
+    private val quizViewModel: QuizViewModel by activityViewModels()
+
+    //ViewBinding
     private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,19 +30,13 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this)[DashboardViewModel::class.java]
+        val quizViewModel =
+            ViewModelProvider(this)[QuizViewModel::class.java]
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        /*
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-         */
-        return root
+
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/practice/gameapp/ui/viewmodels/QuizViewModel.kt
+++ app/src/main/java/com/practice/gameapp/ui/viewmodels/QuizViewModel.kt
@@ -7,7 +7,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel @Inject constructor()
+class QuizViewModel @Inject constructor()
     : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
