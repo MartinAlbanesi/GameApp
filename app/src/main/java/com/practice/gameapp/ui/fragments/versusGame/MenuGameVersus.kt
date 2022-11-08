0427c3093b1c4625f8d5ea9ba8ecd073cac1f13f
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-11-08 15:22:52 -0300

    fixing some bugs ~

--- app/src/main/java/com/practice/gameapp/ui/fragments/versusGame/MenuGameVersus.kt
+++ app/src/main/java/com/practice/gameapp/ui/fragments/versusGame/MenuGameVersus.kt
@@ -37,9 +37,6 @@ class MenuGameVersus : Fragment() {
         Navigation.findNavController(requireView()).navigate(R.id.action_navigation_menugameversus_to_versusFragment)
         }
 
-
-
-
         binding.button.setOnClickListener {
             lifecycleScope.launch {
                 versusViewModel.fillName()
