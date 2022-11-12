7f984aae5d04fb96b2594d018280424ded1afc9b
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-11-12 18:07:05 -0300

    implement clickable in each game in the search game

--- app/src/main/java/com/practice/gameapp/ui/fragments/home/HomeSearch.kt
+++ app/src/main/java/com/practice/gameapp/ui/fragments/home/HomeSearch.kt
@@ -1,12 +1,18 @@
 package com.practice.gameapp.ui.fragments.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -25,7 +31,7 @@ fun HomeSearchGame(
     {
         gameList.forEach {
             if (it.title.startsWith(textGame[0].uppercaseChar())) {
-                GameView(title = it.title, thumbnail = it.thumbnail)
+                GameView(it)
             }
         }
     }
@@ -33,31 +39,68 @@ fun HomeSearchGame(
 
 @Composable
 fun GameView(
-    title : String,
-    thumbnail : String,
-    modifier : Modifier = Modifier
+    gameModel: GameModel,
+    modifier: Modifier = Modifier
 ) {
+
+    var isSize by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val size = if (isSize) 200.dp else 56.dp
+    val sizeImage = if (isSize) 175.dp else 100.dp
+
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .height(52.dp)
+            .height(size)
+            .clickable { isSize = !isSize }
     ) {
 
-        Image(
-            painter = rememberAsyncImagePainter(
-                model = thumbnail
-            ),
-            contentDescription = null
-        )
-        Text(
-            modifier = modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(horizontal = 8.dp, vertical = 12.dp),
-            text = title,
-            color = Color.White,
-            textAlign = TextAlign.Left
-        )
+        Column() {
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = gameModel.thumbnail
+                ),
+                contentDescription = null,
+                modifier = modifier.size(sizeImage),
+                contentScale = ContentScale.FillBounds
+            )
+
+            if (isSize) {
+                Text(
+                    text = gameModel.platform,
+                    textAlign = TextAlign.Center,
+                    color = Color.White
+                )
+            }
+        }
+        Column() {
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 12.dp),
+                text = gameModel.title,
+                color = Color.White,
+                textAlign = TextAlign.Left
+            )
+            if (isSize){
+                Divider(thickness = 1.dp, color = Color.Black)
+                Text(
+                    modifier = Modifier.height(125.dp).padding(horizontal = 8.dp, vertical = 12.dp),
+                    text = gameModel.short_description,
+                    textAlign = TextAlign.Left,
+                    color = Color.White
+                )
+                Divider(thickness = 1.dp, color = Color.Black)
+                Text(
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, top = 4.dp),
+                    text = gameModel.genre,
+                    textAlign = TextAlign.Center,
+                    color = Color.White
+                )
+            }
+        }
     }
-    Divider(thickness = 1.dp, color = Color.Black)
+    Divider(thickness = 2.dp, color = Color.Black)
 }
