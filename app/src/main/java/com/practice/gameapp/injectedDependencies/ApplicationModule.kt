46b7768486dad64bcbab76b593cda5265488d884
Martín Albanesi <martinalbanesi89@gmail.com>
2022-11-14 20:53:59 -0300

    added koin module in ApplicationModule and build in MainApplication

--- app/src/main/java/com/practice/gameapp/injectedDependencies/ApplicationModule.kt
+++ app/src/main/java/com/practice/gameapp/injectedDependencies/ApplicationModule.kt
@@ -6,10 +6,17 @@ import com.practice.gameapp.data.repositories.GameRepository
 import com.practice.gameapp.data.repositories.network.game.GameAPI
 import com.practice.gameapp.data.repositories.network.game.GameAPIClient
 import com.practice.gameapp.data.repositories.network.game.GameClient
+import com.practice.gameapp.ui.fragments.home.HomeFragment
+import com.practice.gameapp.ui.viewmodels.MainViewModel
+import com.practice.gameapp.ui.viewmodels.home.HomeViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.koin.android.compat.ScopeCompat.viewModel
+import org.koin.androidx.fragment.dsl.fragment
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -39,4 +46,15 @@ class ApplicationModule {
     fun provideGameClient(gameAPI: GameAPI): GameClient {
         return GameAPIClient(gameAPI)
     }
+}
+
+val remoteRepositoryModule = module {
+    //single <GameAPIProvider> { GameAPIMainProvider() }
+    single <GameClient> { GameAPIClient(get()) }
+    single <GameRepository> { GameAPIRepository(get()) }
+    fragment { HomeFragment() }
+
+    //ViewModel
+    viewModel { HomeViewModel(get()) }
+    viewModel { MainViewModel() }
 }
--- app/src/main/java/com/practice/gameapp/injectedDependencies/MainApplication.kt
+++ app/src/main/java/com/practice/gameapp/injectedDependencies/MainApplication.kt
@@ -2,8 +2,18 @@ package com.practice.gameapp.injectedDependencies
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
 
 @HiltAndroidApp
 class MainApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication.applicationContext)
+            modules(remoteRepositoryModule)
+        }
+    }
 }
